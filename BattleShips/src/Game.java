869957013c18e3e298df9e11ce58341760d4cee7
James Wallace <74025418+wanderllama@users.noreply.github.com>
2022-03-21 13:52:15 -0400

    working game added display board for user to see their hits/misses. defined range for user input to print message for invalid input

--- BattleShips/src/Game.java
+++ BattleShips/src/Game.java
@@ -81,7 +81,6 @@ public class Game {
             }
             count++;
         }
-
         //change null values to ' ' character for nicer print
         for (int i = 1; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
@@ -92,64 +91,117 @@ public class Game {
                 }
             }
         }
-        //print grid with each array on its own line
-        for (String[] arr : grid) {
+        //following commented out code prints game board with ships locations -> for debugging
+        /*for (String[] arr : grid) {
             System.out.println(Arrays.toString(arr));
-        }
+        }*/
         //print ship positions stored in 2D array
         System.out.println(Arrays.deepToString(ships));
 //end of board print/creation
 
-        String[][] shipGuess = new String[3][3];
+        //game board for user to see hits and misses -> not ship location
+        String[][] board = new String[8][8];
+
+        //add key for display board
+        for (int i = 0; i < board[0].length; i++) {
+            board[0][i] = i + "";
+        }
+        //add capital letter to first index of each row of board starting with 'A'
+        char letterBoard = 'A';
+        for (int i = 1; i < board.length; i++) {
+            board[i][0] = "" + letterBoard++;
+        }
+        //change null space to sine wave unicode character
+        for (int i = 1; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == null) {
+                    board[i][j] = "\u223f";
+                }
+            }
+        }//end of display board setup
+
+        //shipGuess array will hold the corresponding coordinates for each ship
+        //starts with all null values and when hit occurs then add coordinates to array
+        String[][] shipGuess = new String[3][3];//todo determine if i need this data kept track of if not delete
+
+        //displayHit array starts with null values and used to display correct guesses
+        //when hit occurs coordinates are added to first available index
         String[] displayHit = new String[9];
+
+        //shipHit array stores the value for the number of times each ship has been sunk
+        //when element = 3 then display "ship sunk"
         int[] shipHit = {0, 0, 0};
+
+        //String of previously made guesses that are valid
+        //user can ask for this string to be printed
         String attempts = "";
+
+        //hit value is the number of successful hits
+        //game ends when hit = 9
         int hit = 0;
 
         while (hit < 9) {
             //accept user input for coordinate to fire shot at -> A1, B3, C5, G7
-            System.out.println("enter position");
+            System.out.println("enter position\nenter 'S' for previous attempted coordinates");
             String position = input.nextLine().trim().toUpperCase(); //65 ASCII value for 'A' -> 49 ASCII value for 1
 
-            //convert the input so the first character is the row number and the second character is index number of entered coordinate
-            int rowGuess = position.charAt(0) - 64;//store ASCII value for number that corresponds to row number
-            int columnGuess = position.charAt(position.length() - 1) - 48;//store ASCII value for number that corresponds to column(index)
+            if (position.equals("S")) {
+                System.out.println(attempts);
+            } else if (position.charAt(0) >= 65 && position.charAt(0) <= 71 && position.charAt(1) >= 49 && position.charAt(1) <= 55) {
+                //convert the input so the first character is the row number and the second character is index number of entered coordinate
+                int rowGuess = position.charAt(0) - 64;//store ASCII value for number that corresponds to row number
+                int columnGuess = position.charAt(position.length() - 1) - 48;//store ASCII value for number that corresponds to column(index)
 
-            switch (grid[rowGuess][columnGuess]) {
-                case "" + '\u2693':
-                    System.out.println("Hit");
-                    grid[rowGuess][columnGuess] = "2";
-                    for (int i = 0; i < ships.length; i++) {
-                        for (int j = 0; j < ships[i].length; j++) {
-                            if (ships[i][j].equals(position)) {
-                                shipGuess[i][j] = position;
-                                shipHit[i]++;
+                switch (grid[rowGuess][columnGuess]) {
+                    case "" + '\u2693'://coordinates of game board has anchor unicode character as element
+                        System.out.println("Hit");
+                        grid[rowGuess][columnGuess] = "2";//change element value to 2
+                        //determine position of master list of existing ship coordinates and add hit coordinate to shipGuess array
+                        for (int i = 0; i < ships.length; i++) {
+                            for (int j = 0; j < ships[i].length; j++) {
+                                if (ships[i][j].equals(position)) {
+                                    shipGuess[i][j] = position;//add to shipGuess array
+                                    shipHit[i]++;//increase hit counter to correct ship in shipHit array -> each element represents the number of times a hit has occurred on each ship
+                                }
                             }
                         }
-                    }
-                    for (int num : shipHit) {
-                        if (num == 3) {
-                            System.out.println("sunk ship");
+                        //check each element in shipHit array
+                        for (int num : shipHit) {
+                            if (num == 3) {//if element = 3 then print sunk ship
+                                System.out.println("sunk ship");
+                            }
                         }
-                    }
-                    displayHit[hit] = position;
-                    hit++;
-                    break;
-                case " ":
-                    System.out.println("Miss");
-                    grid[rowGuess][columnGuess] = "0";
-                    break;
-                case "2":
-                    System.out.println("already hit boat in that position");
-                    continue;
-                case "0":
-                    System.out.println("already guessed that position");
-                    continue;
+                        //add coordinates to leftmost element in displayHit array so user can see what shot have hit ships
+                        displayHit[hit] = position;
+                        //hit counter increments
+                        hit++;
+                        //add skull and crossbones unicode character to display board for users hit
+                        board[rowGuess][columnGuess] = "\u2620";
+                        break;
+                    case " ":
+                        System.out.println("Miss");
+                        grid[rowGuess][columnGuess] = "0";
+                        //add miss character to display board
+                        board[rowGuess][columnGuess] = "\u2327";
+                        break;
+                    case "2":
+                        System.out.println("already hit boat in that position");
+                        continue;
+                    case "0":
+                        System.out.println("already guessed that position");
+                        continue;
+                }
+            } else {
+                System.out.println("invalid input");
+                continue;
+            }
+            //print board after each guess may move this to only occur when user request and end of game-> todo create method for this to print at end of game and when user enters 'X'
+            for (String[] arr : board) {
+                System.out.println(Arrays.toString(arr));
             }
             System.out.println(Arrays.toString(displayHit));
             attempts += position + " ";//store the previous guesses made by user
         }
-
         System.out.println("Game Over");
     }
 }
